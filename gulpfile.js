584f7d6427f8a0a64610ae83ece02d6a1e11e2c2
Sean Gau <rrtw0627@gmail.com>
2021-09-22 14:38:03 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,44 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const data = require('gulp-data');
 const connect = require('gulp-connect');
+const sitemap = require('gulp-sitemap');
 const fs = require('fs');
 
-sass.compiler = require('node-sass');
+function build() {  
+  let build_time = new Date().getTime();
+  gulp.src('src/sass/**/*.sass')
+  .pipe(data((file) => {
+    console.log("[build] "+file['history']);
+  }))
+  .pipe(sass.sync().on('error', sass.logError))
+  .pipe(gulp.dest('./static/assets/css'));
+
+  gulp.src('src/pug/**/index.pug')
+  .pipe(data((file) => {
+    console.log("[build] "+file['history']);
+    const result = {
+      index: require('./data/index.json'),
+      timestamp: build_time
+    };
+    return result;
+  }))
+  .pipe(pug())
+  .pipe(gulp.dest('./static/'))
+  .pipe(connect.reload());
+}
 
 gulp.task('server', function () {
     connect.server({
         port: 3000,
         livereload: true,
         root: 'static'
-    });
+    })
+    build();
 
-    gulp.src('src/sass/**/*.scss')
-    .pipe(data((file) => {
-      console.log("[build] "+file['history']);
-    }))
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./static/assets/css'));
-
-    gulp.src('src/**/index.pug')
-    .pipe(data((file) => {
-      console.log("[build] "+file['history']);
-      const result = {
-        index: require('./data/index.json'),
-      };
-      return result;
-    }))
-    .pipe(pug())
-    .pipe(gulp.dest('./static/'))
-    .pipe(connect.reload());
-
-    gulp.watch(['src/**/*.pug', 'src/sass/**/*.scss', 'static/assets/js/*.js', 'data/*.json'], function(event){
-
-      gulp.src('src/sass/**/*.scss')
-      .pipe(data((file) => {
-        console.log("[build] "+file['history']);
-      }))
-      .pipe(sass.sync().on('error', sass.logError))
-      .pipe(gulp.dest('./static/assets/css'));
-
-      gulp.src('src/**/index.pug')
-      .pipe(data((file) => {
-        console.log("[build] "+file['history']);
-        const result = {
-          index: require('./data/index.json'),
-        };
-        return result;
-      }))
-      .pipe(pug())
-      .pipe(gulp.dest('./static/'))
-      .pipe(connect.reload());
+    gulp.watch(['src/**/*.pug', 'src/**/*.sass', 'static/assets/js/*.js', 'data/*.json'], function(event){
+      build();
       event();
     });
 });
